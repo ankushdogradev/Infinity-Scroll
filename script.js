3daf567c178d2b47b8c014a7b34a4f2fa3c3eb83
Ankush Dogra <ankushdogradev@gmail.com>
2020-12-22 12:05:51 +0530

    Added infinite scroll
    initially 5 photos will load (it will help page load faster) & after that 30 photos will load every time

--- script.js
+++ script.js
@@ -1,20 +1,82 @@
-//Unsplash API
-const count = 10;
-const apiKey = "AUq-6UdArn3p6iEYxnr24gktSKV8ZKZXq9CZl8VdoLM";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
-//https://unsplash.com/oauth/applications/192563
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+//Unsplash API
+let count = 5;
+const apiKey = "AUq-6UdArn3p6iEYxnr24gktSKV8ZKZXq9CZl8VdoLM";
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true; //Hides loading icn after image loads
+    cunt = 30;
+  }
+}
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+//Create Elements for links & photos, ADD TO DOM
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  //Run functin for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    //Create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    //Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+    // put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 //Get photos from unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {
     //Catch Error
   }
 }
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 //on load
 getPhotos();
